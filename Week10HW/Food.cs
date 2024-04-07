3bd1f3cf9af41501922ce30b8c36fec71dd81d44
Derek <Drj2778@email.vccs.edu>
2024-04-06 21:41:13 -0400

    Add project files.

---
+++ Week10HW/Food.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week10HW
+{
+    class Food
+    {
+        int foodId;
+        string foodName, foodType;
+        double foodAmount;
+
+        public int FoodId 
+        {
+         get { return foodId; }
+         set { foodId = value; }      
+        }
+
+        public string FoodName 
+        {
+            get { return foodName; }
+            set { foodName = value; }
+            
+        }
+        public string FoodType
+        {
+            get { return foodType; }
+            set { foodType = value; }
+
+
+        }
+        public double FoodAmount 
+        {
+            get { return foodAmount; }
+            set {foodAmount = value; }
+        
+        
+        }
+    }
+}
---
+++ Week10HW/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace Week10HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Food food1 = new Food();
+            Food food2 = new Food();
+          
+
+
+            food1.FoodId = 1234;
+            food1.FoodName = "Patty Paradise";
+            food1.FoodType = "Burger";
+            food1.FoodAmount = 20.00;
+
+            food2.FoodId = 1022;
+            food2.FoodName = "Cold Turkey";
+            food2.FoodType = "Turkey Sandwhich";
+            food2.FoodAmount = 15.00;
+
+            Console.WriteLine("{0,20},{1,20},{2,20},{3,20}", "FoodId", "FoodName", "FoodType", "FoodAmount");
+
+            Console.WriteLine("{0,20},{1,20},{2,20},{3,20}", food1.FoodId, food1.FoodName, food1.FoodType, food1.FoodAmount);
+            Console.WriteLine("{0,20},{1,20},{2,20},{3,20}", food2.FoodId, food2.FoodName, food2.FoodType, food2.FoodAmount);
+
+            ReadKey();
+
+
+        }
+    }
+}
